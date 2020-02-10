a6c98ac8c4d6ac9ddb764d92f45ccecc831227e6
kalpriksh <kalprikshbisst@gmail.com>
2020-02-10 09:23:31 +0530

    added home component, modified login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from "@angular/router";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatInputModule} from '@angular/material/input';
-import { LoginComponent } from './login/login.component';
 
+import { MatInputModule } from '@angular/material/input';
+import { MatSliderModule } from '@angular/material/slider';
+
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+
+import { ReactiveFormsModule,FormsModule  } from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
     MatInputModule,
-    BrowserAnimationsModule
+    MatSliderModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      {
+        path:'login',
+        component:LoginComponent
+      },
+      {
+        path:'home',
+        component:HomeComponent
+      }
+    ])    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
